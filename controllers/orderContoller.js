39208a150baaeafe5b86017249a1afacaeaa2cb0
unknown <iqbalraju123@gmail.com>
2023-03-08 00:30:47 +0600

    order controller error fixed

--- controllers/orderContoller.js
+++ controllers/orderContoller.js
@@ -43,7 +43,7 @@ exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
   );
 
   if (!order) {
-    return next(new ErrorHander("Order not found with this Id", 404));
+    return next(new ErrorHandler("Order not found with this Id", 404));
   }
 
   res.status(200).json({
@@ -84,11 +84,11 @@ exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
   const order = await Order.findById(req.params.id);
 
   if (!order) {
-    return next(new ErrorHander("Order not found with this Id", 404));
+    return next(new ErrorHandler("Order not found with this Id", 404));
   }
 
   if (order.orderStatus === "Delivered") {
-    return next(new ErrorHander("You have already delivered this order", 400));
+    return next(new ErrorHandler("You have already delivered this order", 400));
   }
 
   if (req.body.status === "Shipped") {
@@ -121,7 +121,7 @@ exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
   const order = await Order.findById(req.params.id);
 
   if (!order) {
-    return next(new ErrorHander("Order not found with this Id", 404));
+    return next(new ErrorHandler("Order not found with this Id", 404));
   }
 
   await order.remove();
