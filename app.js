8a994d9de5b19e54d106b5be1bac1066ee7b6a35
unknown <iqbalraju123@gmail.com>
2023-03-15 21:49:41 +0600

    modify productroute

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser')
+const cors = require('cors');
 
 //route
 const userRoute = require("./routes/userRoute");
@@ -10,6 +11,7 @@ const productRoute = require('./routes/productRoute');
 const orderRoute = require('./routes/orderRoute');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -13,7 +13,8 @@ const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route("/products").get(isAuthenticatedUser,authorizeRoles("admin"),getAllProducts);
+router.route("/products").get(getAllProducts);
+//router.route("/products").get(isAuthenticatedUser,authorizeRoles("admin"),getAllProducts);
 router.route("/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
 router.route("/product/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct).get(getProductDetails);
 router.route("/review").put(isAuthenticatedUser, createProductReview);
